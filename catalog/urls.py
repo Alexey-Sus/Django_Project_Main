97fad20264d1399c9b4da3610e202bc98820de84
Al_Pl <asuspitsyn@mail.ru>
2025-04-15 23:35:36 +0300

    added many features such as: 1. Created a new view for a product to render the product's card with all relevant product's info. 2. Created a main page template which renders all products by means of a cycle, i.e., product cards are rendered with the 'for... endfor' cycle. 3. Created a base template and an upper menu subtemplate.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from catalog.apps import NewappConfig
-from catalog.views import home, contact_details
+from catalog.views import home, contact_details, product_details, main, prod_det_from_base, upper_menu
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,7 +8,11 @@ app_name = NewappConfig.name
 
 urlpatterns = [
     path('home/', home, name='home'),
-    path('contacts/', contact_details, name='contact_details')
+    path('contacts/', contact_details, name='contact_details'),
+    path('product_details/<int:pk>/', product_details, name='product_details'),
+    path('prod_det_from_base/<int:pk>/', prod_det_from_base, name='prod_det_from_base'),
+    path('main/', main, name='main'),
+    path('upper_menu_subtempl/', upper_menu, name='upper_menu_subtempl'),
 ]
 
 if settings.DEBUG:
